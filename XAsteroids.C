95c039a41c6d3d1d5f3553ad92f380b8c6ba0f3f
Charles Archer <charles.archer@cornelisnetworks.com>
2026-08-23 17:18:43 -0500

    refactor: inline header static members (ODR audit zero) + construct the 11 globals by pointer in main() (D11 seam prep)

--- XAsteroids.C
+++ XAsteroids.C
@@ -1,4 +1,5 @@
 #include<stdlib.h>
+#include<new>
 #include<iostream>
 #include"gamePlay/stage.H"
 #include"gamePlay/score.H"
@@ -10,28 +11,57 @@
 #include"objects/enemies/enemyGroup.H"
 #include"objects/enemies/enemyBulletGroup.H"
 #include"gamePlay/playingField.H"
-Stage stage;
-Button button(stage.display,stage.window,
-              "Options",stage.buttonInfo,
-              stage.buttonFg,stage.buttonBg,
-              stage.buttonX,stage.buttonY);
-Score score;
-ExplosionGraphic explosionGraphic;
-ShipGroup shipGroup;
-ShipYard shipYard(stage.display,stage.window,
-		              shipGroup.ship->icon,
-		              shipGroup.ship->iconWidth,shipGroup.ship->iconHeight,
-                      shipGroup.ship->iconColor,
-		              stage.shipYardWidth,stage.shipYardHeight,Stage::shipYardBg);
-ShipBulletGroup shipBulletGroup;
-RockGroup rockGroup;
-EnemyGroup enemyGroup;
-EnemyBulletGroup enemyBulletGroup;
-PlayingField playingField;
+Stage* stage = nullptr;
+Button* button = nullptr;
+Score* score = nullptr;
+ExplosionGraphic* explosionGraphic = nullptr;
+ShipGroup* shipGroup = nullptr;
+ShipYard* shipYard = nullptr;
+ShipBulletGroup* shipBulletGroup = nullptr;
+RockGroup* rockGroup = nullptr;
+EnemyGroup* enemyGroup = nullptr;
+EnemyBulletGroup* enemyBulletGroup = nullptr;
+PlayingField* playingField = nullptr;
 
 int main (int argc, char *argv[])
- {cout<<"Your highest score this game was "<<playingField.PlayTheGame(argc>1 ? atoi(argv[1])
-                                                                             : 1,
-                                                                      argc, argv)<<'.'<<endl;
+ {stage = new Stage;
+  button = new Button(stage->display,stage->window,
+                      "Options",stage->buttonInfo,
+                      stage->buttonFg,stage->buttonBg,
+                      stage->buttonX,stage->buttonY);
+  score = new Score;
+  explosionGraphic = new ExplosionGraphic;
+  shipGroup = (ShipGroup*)::operator new(sizeof(ShipGroup));
+  ShipGroup::ship=&shipGroup->starDestroyer;
+  ShipGroup::thrust=&shipGroup->starDestroyerThrust;
+  new (shipGroup) ShipGroup;
+  shipYard = new ShipYard(stage->display,stage->window,
+                          shipGroup->ship->icon,
+                          shipGroup->ship->iconWidth,shipGroup->ship->iconHeight,
+                          shipGroup->ship->iconColor,
+                          stage->shipYardWidth,stage->shipYardHeight,Stage::shipYardBg);
+  shipBulletGroup = (ShipBulletGroup*)::operator new(sizeof(ShipBulletGroup));
+  new (shipBulletGroup) ShipBulletGroup;
+  rockGroup = new RockGroup;
+  enemyGroup = new EnemyGroup;
+  enemyBulletGroup = (EnemyBulletGroup*)::operator new(sizeof(EnemyBulletGroup));
+  new (enemyBulletGroup) EnemyBulletGroup;
+  playingField = new PlayingField;
+
+  cout<<"Your highest score this game was "<<playingField->PlayTheGame(argc>1 ? atoi(argv[1])
+                                                                                    : 1,
+                                                                             argc, argv)<<'.'<<endl;
+
+  delete playingField;
+  delete enemyBulletGroup;
+  delete enemyGroup;
+  delete rockGroup;
+  delete shipBulletGroup;
+  delete shipYard;
+  delete shipGroup;
+  delete explosionGraphic;
+  delete score;
+  delete button;
+  delete stage;
   return 0;
  }
